239c8c38fbc42ba45b660e9bf80044de9f524d1f
shopkins <shopkins@openmethods.com>
2014-08-21 13:58:35 -0500

    *Rebuilt the wizard used for creating Business Objects to work with Package Explorer

--- desktop/plugins/org.eclipse.vtp.desktop.projects.core/src/main/java/org/eclipse/vtp/desktop/projects/core/wizards/CreateBusinessObjectWizard.java
+++ desktop/plugins/org.eclipse.vtp.desktop.projects.core/src/main/java/org/eclipse/vtp/desktop/projects/core/wizards/CreateBusinessObjectWizard.java
@@ -11,6 +11,14 @@
  -------------------------------------------------------------------------*/
 package org.eclipse.vtp.desktop.projects.core.wizards;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
@@ -20,6 +28,8 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.layout.FormAttachment;
@@ -28,12 +38,17 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.vtp.desktop.model.core.internal.OpenVXMLProject;
 
 import com.openmethods.openvxml.desktop.model.businessobjects.IBusinessObject;
+import com.openmethods.openvxml.desktop.model.businessobjects.IBusinessObjectProjectAspect;
 import com.openmethods.openvxml.desktop.model.businessobjects.IBusinessObjectSet;
 
 /**
@@ -49,15 +64,12 @@ import com.openmethods.openvxml.desktop.model.businessobjects.IBusinessObjectSet
 public class CreateBusinessObjectWizard extends Wizard implements INewWizard,
 IExecutableExtension
 {
-	/**
-	 * The business object set that will contain the new business object.
-	 */
-	private IBusinessObjectSet objectSet = null;
-
+	private Map<String,ProjectProperties> projectPropertiesList = new HashMap<String,ProjectProperties>();
+	
 	/**
 	 * The wizard page that collects the name of the new business object.
 	 */
-	private BusinessObjectWizardPage bwp = null;
+	private BusinessObjectWizardPage boWizPage = null;
 
 	IConfigurationElement configElement = null;
 	
@@ -71,8 +83,8 @@ IExecutableExtension
 	public CreateBusinessObjectWizard()
 	{
 		super();
-		bwp = new BusinessObjectWizardPage();
-		addPage(bwp);
+		boWizPage = new BusinessObjectWizardPage();
+		addPage(boWizPage);
 	}
 
 	/* (non-Javadoc)
@@ -89,15 +101,71 @@ IExecutableExtension
 	 */
 	public void init(IWorkbench workbench, IStructuredSelection selection)
 	{
-		Object obj = selection.getFirstElement();
-		if(obj == null)
+		//set up projectPropertiesList
+		//get all the projects in the workspace
+		IProject[] projs = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+
+		//set up natures for project type recognition and display
+		String workflowNature = org.eclipse.vtp.desktop.model.core.natures.WorkflowProjectNature.NATURE_ID;
+		String workflowNature5 = org.eclipse.vtp.desktop.model.core.natures.WorkflowProjectNature5_0.NATURE_ID;
+		String applicationNature = org.eclipse.vtp.desktop.projects.core.builder.VoiceApplicationNature.NATURE_ID;//legacy type
+		String fragmentNature = org.eclipse.vtp.desktop.projects.core.builder.VoiceApplicationFragmentNature.NATURE_ID;//legacy type
+		
+		//TODO fix project setup and access the static NATURE_ID for these
+//		String voiceNature = org.eclipse.vtp.desktop.model.interactive.voice.natures.VoiceProjectNature.NATURE_ID;
+//		String voiceNature5 = org.eclipse.vtp.desktop.model.interactive.voice.natures.VoiceProjectNature5_0.NATURE_ID;
+//		String personaNature = org.eclipse.vtp.desktop.projects.voice.builder.VoicePersonaNature.NATURE_ID;//legacy type
+		String voiceNature = "org.eclipse.vtp.desktop.model.interactive.voice.VoiceProjectNature";
+		String voiceNature5 = "org.eclipse.vtp.desktop.model.interactive.voice.VoiceProjectNature5_0";
+		String personaNature = "org.eclipse.vtp.desktop.projects.voice.VoicePersonaNature";//legacy type
+		
+		for(IProject proj : projs)
 		{
-			obj = ((IStructuredSelection)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection()).getFirstElement();
-		}
-		if(obj instanceof IBusinessObjectSet)
-			this.objectSet = (IBusinessObjectSet)obj;
-		else if(obj instanceof IBusinessObject)
-			this.objectSet = ((IBusinessObject)obj).getBusinessObjectSet();
+			ProjectProperties projProps = new ProjectProperties();
+			
+			//set the project name
+			projProps.setName(proj.getName());
+			
+			//set the project type
+			try{
+			if(proj.getNature(workflowNature)!= null)
+				projProps.setType("Workflow");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(workflowNature5)!= null)
+				projProps.setType("Workflow");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(voiceNature)!= null)
+				projProps.setType("Voice");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(voiceNature5)!= null)
+				projProps.setType("Voice");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(applicationNature)!= null)
+				projProps.setType("(Legacy) Application");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(fragmentNature)!= null)
+				projProps.setType("(Legacy) Fragment");
+			}catch(Exception e){e.printStackTrace();}
+			try{
+			if(proj.getNature(personaNature)!= null)
+				projProps.setType("(Legacy) Voice");
+			}catch(Exception e){e.printStackTrace();}
+
+			//set the business object set
+			if ("Workflow".equals(projProps.getType()))
+			{
+				OpenVXMLProject ovxmlProj = new OpenVXMLProject(proj);
+				String aspectID = com.openmethods.openvxml.desktop.model.businessobjects.IBusinessObjectProjectAspect.ASPECT_ID;
+				IBusinessObjectProjectAspect boProjAspect = (IBusinessObjectProjectAspect) ovxmlProj.getProjectAspect(aspectID);
+				projProps.setBoSet(boProjAspect.getBusinessObjectSet());
+			}
+			this.projectPropertiesList.put(projProps.getName(), projProps);
+		}			
 	}
 	
 	/*
@@ -109,7 +177,11 @@ IExecutableExtension
 	{
 		try
 		{
-			IBusinessObject bo = objectSet.createBusinessObject(bwp.brandNameField.getText());
+			String workflowName = boWizPage.getSelectedWorkflowProjectName();
+			ProjectProperties projProps = projectPropertiesList.get(workflowName);
+			IBusinessObjectSet objectSet = projProps.getBoSet();
+			
+			IBusinessObject bo = objectSet.createBusinessObject(boWizPage.boNameField.getText());
 			objectSet.refresh();
 			IDE.openEditor(PlatformUI.getWorkbench()
 					  .getActiveWorkbenchWindow()
@@ -124,17 +196,22 @@ IExecutableExtension
 
 		return false;
 	}
-
+	
 	private class BusinessObjectWizardPage extends WizardPage
 	{
-		Text brandNameField = null;
-
+		private static final String MESSAGE_NO_WORKFLOW_SELECTED = "Please select a Workflow project for this Business Object.";
+		private static final String MESSAGE_EMPTY_BO_NAME = "Business Object name cannot be empty";
+		private static final String MESSAGE_BO_NAME_TAKEN = "A Business Object already exists with that name.";
+		
+		Text boNameField = null;
+		Tree tree = null;
+		
 		public BusinessObjectWizardPage()
 		{
 			super("BusinessObjectPage",
 				"Enter a name for the new business object", null);
 		}
-
+		
 		/* (non-Javadoc)
 		 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
 		 */
@@ -142,20 +219,28 @@ IExecutableExtension
 		{
 			setPageComplete(false);
 
+			//base composite to build on
 			Composite comp = new Composite(parent, SWT.NONE);
-			Label brandNameLabel = new Label(comp, SWT.NONE);
-			brandNameLabel.setText("Business Object Name:");
-			brandNameLabel.setSize(brandNameLabel.computeSize(SWT.DEFAULT,
+			
+			tree = createProjSelector(comp);
+			
+			//name label
+			Label boNameLabel = new Label(comp, SWT.NONE);
+			boNameLabel.setText("Business Object Name:");
+			boNameLabel.setSize(boNameLabel.computeSize(SWT.DEFAULT,
 					SWT.DEFAULT));
-			brandNameField = new Text(comp, SWT.SINGLE | SWT.BORDER);
-			brandNameField.addVerifyListener(new VerifyListener()
+			
+			//name field
+			boNameField = new Text(comp, SWT.SINGLE | SWT.BORDER);
+			
+			boNameField.addVerifyListener(new VerifyListener()
 			{
 
 				public void verifyText(VerifyEvent e)
                 {
 	                String text = e.text;
 	                char[] chars = text.toCharArray();
-					String currentName = brandNameField.getText().substring(0, e.start) + e.text + brandNameField.getText(e.end, (brandNameField.getText().length() - 1));
+					String currentName = boNameField.getText().substring(0, e.start) + e.text + boNameField.getText(e.end, (boNameField.getText().length() - 1));
 	                if(currentName.length() > 255)
 	                {
 	                	e.doit = false;
@@ -178,44 +263,250 @@ IExecutableExtension
                 }
 				
 			});
-			brandNameField.addModifyListener(new ModifyListener()
+			boNameField.addModifyListener(new ModifyListener()
 				{
 					public void modifyText(ModifyEvent e)
 					{
-						String n = brandNameField.getText();
-						for(IBusinessObject bo : objectSet.getBusinessObjects())
+						if(!isWorkflowProjectSelected())
 						{
-							if(bo.getName().equals(n))
-							{
-								setErrorMessage(
-									"A Business Object already exists with that name.");
-								setPageComplete(false);
-
-								return;
-							}
+							setErrorMessage(MESSAGE_NO_WORKFLOW_SELECTED);
+							setPageComplete(false);
+							return;
+						}
+						
+						if(boNameField.getText() == null || boNameField.getText().equals(""))
+						{
+							setErrorMessage(MESSAGE_EMPTY_BO_NAME);
+							setPageComplete(false);
+							return;
+						}
+						
+						if(!isBoNameAvailable())
+						{
+							setErrorMessage(MESSAGE_BO_NAME_TAKEN);
+							setPageComplete(false);
+							return;
 						}
 
 						setErrorMessage(null);
 						setPageComplete(true);
 					}
 				});
+			
+			//Layout section
 			comp.setLayout(new FormLayout());
 
-			FormData brandNameLabelData = new FormData();
-			brandNameLabelData.left = new FormAttachment(0, 10);
-			brandNameLabelData.top = new FormAttachment(0, 30);
-			brandNameLabelData.right = new FormAttachment(0,
-					10 + brandNameLabel.getSize().x);
-			brandNameLabelData.bottom = new FormAttachment(0,
-					30 + brandNameLabel.getSize().y);
-			brandNameLabel.setLayoutData(brandNameLabelData);
+			FormData treeData = new FormData();
+			treeData.left = new FormAttachment(0, 10);
+			treeData.top = new FormAttachment(0, 30);
+			treeData.right = new FormAttachment(100, 10);
+			treeData.bottom = new FormAttachment(75,30);
+			tree.setLayoutData(treeData);
 
-			FormData brandNameFieldData = new FormData();
-			brandNameFieldData.left = new FormAttachment(brandNameLabel, 10);
-			brandNameFieldData.top = new FormAttachment(0, 29);
-			brandNameFieldData.right = new FormAttachment(100, -10);
-			brandNameField.setLayoutData(brandNameFieldData);
+			FormData boNameLabelData = new FormData();
+			boNameLabelData.left = new FormAttachment(0, 10);
+			boNameLabelData.top = new FormAttachment(tree, 30);
+			boNameLabelData.right = new FormAttachment(0, 10 + boNameLabel.getSize().x);
+			boNameLabelData.bottom = new FormAttachment(100, 30 + boNameLabel.getSize().y);
+			boNameLabel.setLayoutData(boNameLabelData);
+
+			FormData boNameFieldData = new FormData();
+			boNameFieldData.left = new FormAttachment(boNameLabel, 10);
+			boNameFieldData.top = new FormAttachment(tree, 30);
+			boNameFieldData.right = new FormAttachment(100, 10);
+			boNameLabelData.bottom = new FormAttachment(100, 30 + boNameField.getSize().y);
+			boNameField.setLayoutData(boNameFieldData);
+				
 			setControl(comp);
 		}
+		
+		/**
+		 * Creates a Tree to use for viewing available projects and selecting which
+		 * Workflow will contain the new Business Object
+		 * 
+		 * @param comp - the Composite on which to put the Tree
+		 * @return a reference to the Tree
+		 */
+		private Tree createProjSelector(Composite comp)
+		{
+			//create the tree
+			Tree tree = new Tree(comp, SWT.SINGLE | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+			tree.setHeaderVisible(true);
+
+			tree.addSelectionListener(new SelectionListener()
+			{
+				@Override
+				public void widgetSelected(SelectionEvent e)
+				{
+					if(!isWorkflowProjectSelected())
+					{
+						setErrorMessage(MESSAGE_NO_WORKFLOW_SELECTED);
+						setPageComplete(false);
+						return;
+					}
+					
+					if(boNameField.getText() == null || boNameField.getText().equals(""))
+					{
+						setErrorMessage(MESSAGE_EMPTY_BO_NAME);
+						setPageComplete(false);
+						return;
+					}
+					
+					if(!isBoNameAvailable())
+					{
+						setErrorMessage(MESSAGE_BO_NAME_TAKEN);
+						setPageComplete(false);
+						return;
+					}
+
+					setErrorMessage(null);
+					setPageComplete(true);
+					
+				}
+
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e)
+				{
+					Object srcObj = e.getSource();
+					if(!(srcObj instanceof Tree))
+						return;
+					
+					Tree tree = (Tree)srcObj;
+					if(tree.getSelectionCount() != 1)
+						return;
+					
+					TreeItem selection = tree.getSelection()[0];
+					if(selection.getItems().length > 0)
+						selection.setExpanded(!selection.getExpanded());
+				}
+			});
+			
+			//set up the columns
+			TreeColumn nameColumn = new TreeColumn(tree, SWT.LEFT);
+			nameColumn.setText("Project Name");
+			nameColumn.setWidth(260);
+			TreeColumn typeColumn = new TreeColumn(tree, SWT.CENTER);
+			typeColumn.setText("Type");
+			typeColumn.setWidth(130);
+			TreeColumn boColumn = new TreeColumn(tree, SWT.RIGHT);
+			boColumn.setText("Business Objects");
+			boColumn.setWidth(210);
+
+			Iterator<String> i = projectPropertiesList.keySet().iterator();
+			while(i.hasNext())
+			{
+				ProjectProperties projProps = projectPropertiesList.get(i.next());
+				
+				String projBoCount = "N/A";
+				List <IBusinessObject> bos = new ArrayList<IBusinessObject>();
+				
+				if ("Workflow".equals(projProps.getType()))
+				{
+					bos = projProps.getBoSet().getBusinessObjects();
+					projBoCount = bos.size() + " Business Object" + (bos.size() == 1 ? "" : "s");
+				}
+				
+				//create the tree item
+				TreeItem projItem = new TreeItem(tree, SWT.NONE);
+				projItem.setText(new String[] {projProps.getName(),projProps.getType(), projBoCount});
+				for(IBusinessObject bo : bos)
+				{
+					TreeItem boItem = new TreeItem(projItem, SWT.NONE);
+					boItem.setText(new String[] {"-","Business Object",bo.getName()});
+				}
+			}
+
+			return tree;
+		}
+
+		/**
+		 * 
+		 * @return The name of the currently selected workflow project in the tree or 
+		 * null if the selection is empty, multiple, or a not a workflow or one of its members
+		 */
+		private String getSelectedWorkflowProjectName()
+		{
+			//Make sure one item is selected
+			if(tree.getSelection().length != 1)
+				return null;
+			
+			//Get the selection and make sure it's a project
+			TreeItem selection = tree.getSelection()[0];
+			if("Business Object".equals(selection.getText(1)))
+				selection = selection.getParentItem();
+			
+			//is it a workflow?
+			if("Workflow".equals(selection.getText(1)))
+				return selection.getText(0);
+			
+			return null;
+		}
+		
+		/**
+		 * 
+		 * @return true if a workflow project or one of its members is selected
+		 */
+		private boolean isWorkflowProjectSelected()
+		{
+			if(getSelectedWorkflowProjectName() != null)
+				return true;
+			return false;
+		}
+		
+		/**
+		 * 
+		 * @return true if a workflow project is selected and the
+		 * proposed name is neither empty nor taken
+		 */
+		private boolean isBoNameAvailable()
+		{
+			String proposedBoName = boNameField.getText();
+			if(proposedBoName == null || proposedBoName.equals(""))
+				return false;
+			
+			//get the workflowname
+			String workflowName = getSelectedWorkflowProjectName();
+			if(workflowName == null)
+				return false;
+			
+			//Is the proposed name taken
+			ProjectProperties projProps = projectPropertiesList.get(workflowName);
+			for(IBusinessObject bo : projProps.getBoSet().getBusinessObjects())
+			{
+				if(bo.getName().equalsIgnoreCase(proposedBoName))
+					return false;
+			}
+
+			return true;
+		}
+		
+		
 	}
+	
+	class ProjectProperties
+	{
+		private String name = "";
+		private String type = "Other";
+		private IBusinessObjectSet boSet = null;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
+		public IBusinessObjectSet getBoSet() {
+			return boSet;
+		}
+		public void setBoSet(IBusinessObjectSet boSet) {
+			this.boSet = boSet;
+		}
+	}
+	
 }
